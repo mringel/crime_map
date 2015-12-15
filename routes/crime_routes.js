969b21385cf5e78a2e925a943f01d8699a8b1bc0
Dustin Noyes <dustin.noyes@gmail.com>
2015-12-15 09:41:10 -0800

    twitter api

--- routes/crime_routes.js
+++ routes/crime_routes.js
@@ -2,11 +2,19 @@ var express = require('express');
 var handleError = require(__dirname + '/../lib/handleServerError');
 var Crime = require(__dirname + '/../models/crime');
 var requester = require('request');
+var Twitter = require('twitter');
 var crimeRouter = module.exports = exports = express.Router();
 
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_TOKEN_SECRET
+});
 
-crimeRouter.get('/externalCall/:month', function(req, res) {
-  var result = callAPI(req.params.month, function(err, body){
+
+crimeRouter.get('/externalSPD/:month', function(req, res) {
+  var result = callSPD(req.params.month, function(err, body){
     var incidents = JSON.parse(body).features;
     for(var i=0; i<incidents.length; i++){
       var newCrime = new Crime(incidents[i]);
@@ -18,7 +26,13 @@ crimeRouter.get('/externalCall/:month', function(req, res) {
   });
 });
 
-function callAPI (search, cb){
+crimeRouter.get('/externalTWITTER/:month', function(req, res) {
+  var result = callTwitter();
+  res.json('');
+});
+
+
+function callSPD (search, cb){
     requester('https://data.seattle.gov/resource/y7pv-r3kh.geojson?month=' + search, function (error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log('socrata api called')
@@ -26,3 +40,19 @@ function callAPI (search, cb){
         typeof cb === 'function' && cb(null, body);
     });
 };
+
+var params = {screen_name: 'nodejs'};
+
+function callTwitter (){
+  console.log(client.consumer_key);
+  client.get('statuses/user_timeline', params, function(error, tweets, response){
+    if(error){
+      console.log(error);
+    }
+    if (!error) {
+      console.log(tweets);
+    }
+});
+}
+
+
