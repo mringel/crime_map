5139e3f8ed9b584604904ac56f57a70cf90d2e54
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-12-15 20:43:51 -0800

    added db to gitignore

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -8,9 +8,45 @@ var crimeMapApp = angular.module('CrimeMapApp', ['leaflet-directive']);
 // require('./directives/directives')(crimeMapApp);
 
 crimeMapApp.controller("SimpleMapController", [ '$scope', function($scope) {
+  var tilesDict = {
+    openstreetmap: {
+              url: "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+              options: {
+                  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              }
+          },
+    opencyclemap: {
+              url: "http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png",
+              options: {
+                  attribution: 'All maps &copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, map data &copy; <a href="http://www.openstreetmap.org">OpenStreetMap</a> (<a href="http://www.openstreetmap.org/copyright">ODbL</a>'
+              }
+          },
+    mapbox_dark: {
+              name: 'Mapbox Dark',
+              url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+              type: 'xyz',
+              options: {
+                  apikey: process.env.MAPBOX_PUBLIC_TOKEN,
+                  mapid: 'mapboox.dark'
+              }
+          },
+    mapbox_wheat: {
+              name: 'Mapbox Wheat Paste',
+              url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+              type: 'xyz',
+              options: {
+                  apikey: 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+                  mapid: 'bufanuvols.lia35jfp'
+              }
+          }
+      };
+
+
     angular.extend($scope, {
         defaults: {
             scrollWheelZoom: false
-        }
+        },
+        tiles: tilesDict.mapbox_dark
     });
+
 }]);
